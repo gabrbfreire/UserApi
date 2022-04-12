6b1594a6b9379f7140ffb0225be37bf367f8c834
Gabriel Freire <gabrbfreire@gmail.com>
2022-04-12 14:02:55 -0300

    Created TokenService

---
+++ UserApi/Services/TokenService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using UserApi.Models;
+
+namespace UserApi.Services
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> user)
+        {
+            Claim[] userClaims = new Claim[]
+            {
+                new Claim("username", user.UserName),
+                new Claim("id", user.Id.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret"));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(claims: userClaims, signingCredentials: credentials, expires: DateTime.UtcNow.AddMinutes(30));
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+            return new Token(tokenString);
+        }
+    }
+}
