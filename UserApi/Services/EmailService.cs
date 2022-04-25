9d75bdfc420d955bdd80f7b653f3a3681dbbb833
Gabriel Freire <gabrbfreire@gmail.com>
2022-04-25 13:38:54 -0300

    EmailService changes

--- UserApi/Services/EmailService.cs
+++ UserApi/Services/EmailService.cs
@@ -20,33 +20,38 @@ namespace UserApi.Services
 
         public void SendConfirmationEmail(int userId, string userEmail, string activationCode)
         {
+            var emailMessage = CreateEmailMessage(userId, userEmail, activationCode);
+
+            using var client = new SmtpClient();
+            try
+            {
+                client.Connect(_configuration.GetValue<string>("EmailSettings:SmtpServer"), _configuration.GetValue<int>("EmailSettings:Port"), true);
+                client.AuthenticationMechanisms.Remove("XOUATH2");
+                client.Authenticate(_userApiEmail, System.Environment.GetEnvironmentVariable("UserApiEmailPassword"));
+                client.Send(emailMessage);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
+        }
+
+        private MimeMessage CreateEmailMessage(int userId, string userEmail, string activationCode)
+        {
             var emailMessage = new MimeMessage();
             emailMessage.From.Add(new MailboxAddress("", _userApiEmail));
             emailMessage.To.Add(new MailboxAddress("", userEmail));
             emailMessage.Subject = "Confirmation Link";
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { 
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
                 Text = $"https://localhost:6001/activate/{userId}/{activationCode}"
             };
-
-            using(var client = new SmtpClient())
-            {
-                try
-                {
-                    client.Connect(_configuration.GetValue<string>("EmailSettings:SmtpServer"), _configuration.GetValue<int>("EmailSettings:Port"), true);
-                    client.AuthenticationMechanisms.Remove("XOUATH2");
-                    client.Authenticate(_userApiEmail, System.Environment.GetEnvironmentVariable("UserApiEmailPassword"));
-                    client.Send(emailMessage);
-                }
-                catch
-                {
-                    throw;
-                }
-                finally
-                {
-                    client.Disconnect(true);
-                    client.Dispose();
-                }
-            }
+            return emailMessage;
         }
     }
 }
